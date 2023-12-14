46a68fb429ba28f23d1c1ee8aa29dfa74dc8ec95
GavinPras <136899300+GavinPras@users.noreply.github.com>
2023-12-14 08:09:24 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer({ items }) {
+  if (items.length === 0) {
+    return <footer className="stats">Barang belanjaan masih kosong !</footer>;
+  }
+  const totalItems = items.length;
+  const checkedItems = items.filter((item) => item.checked).length;
+  const percetage = Math.round((checkedItems / totalItems) * 100);
+  return (
+    <footer className="stats">
+      Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percetage}%)
+    </footer>
+  );
+}
